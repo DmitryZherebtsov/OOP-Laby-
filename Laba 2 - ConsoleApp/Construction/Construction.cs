a06ac4cfba6161eacfb6954acb3f4898b49328f6
DmitryZherebtsov <79690647+DmitryZherebtsov@users.noreply.github.com>
2023-10-25 13:25:52 +0200

    Laba 2 25.10

---
+++ Laba 2 - ConsoleApp/Construction/Construction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_2___ConsoleApp.Construction
+{
+    class Construction
+    {
+        public Construction(float builders, float height, float width, int entrances, int humanCapacity, string buildMaterial)
+        {
+            Builders = builders;
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial;
+        }
+
+        public float Builders { get; set; }
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get; set; }
+        
+    }
+}
---
+++ Laba 2 - ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Laba_2___ConsoleApp.Construction;
+using System;
+
+
+namespace Laba2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var constr = new Construction(75, 32, 20, 15, 7, "Wood");
+            Console.WriteLine(constr.Builders);
+            Console.WriteLine(constr.BuildMaterial);
+        }
+    }
+}
+
